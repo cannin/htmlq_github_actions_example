7cfdd8b358b44591f450113aa2b478fe0850d8b7
Augustin Luna <cannin@gmail.com>
2022-11-07 15:10:44 -0500

    Make exit 0

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-curl --silent https://fifafanfestivalmx.com | htmlq  --text 'p[style="text-align: center; color: #fec525; font-size: 30px;"]' | grep "Registro Próxiente" && exit 0 || exit 1
+curl --silent https://fifafanfestivalmx.com | htmlq  --text 'p[style="text-align: center; color: #fec525; font-size: 30px;"]' | grep "Registro Próximamente" && exit 0 || exit 1
